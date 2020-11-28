6d1672c2067f6fbb316b4a3326d6d088c327a93a
Winson Tay <weeshern90@gmail.com>
2020-11-28 11:57:23 +0800

    added route

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-isFinite
-
+import { createRouter, createWebHistory } from "vue-router";
+isFinite;
 
 const routes = [
+  { path: "/", redirect: "/coaches" },
   {
-    path: '/',
-    name: 'Home',
-
+    path: "/coaches",
+    component: null,
   },
-]
+  {
+    path: "/coaches/:id",
+    component: null,
+    children: [{ path: "contact", component: null }],
+  },
+  {
+    path: "/register",
+    component: null,
+  },
+  {
+    path: "requests",
+    component: null,
+  },
+  //Not found page
+  {
+    path: "/:notFound(.*)",
+    component: null,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
