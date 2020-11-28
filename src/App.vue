db1518c0fabf57eda8192e8775bdc86ff97ca102
Winson Tay <weeshern90@gmail.com>
2020-11-28 11:48:14 +0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
   </div>
-  <router-view/>
+  <router-view></router-view>
 </template>
 
 <style>
